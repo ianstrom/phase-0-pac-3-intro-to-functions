3f35a7864e84a66e6843bdaeb107874f4450478d
ianstrom <iankstrom@gmail.com>
2022-10-28 14:47:07 -0700

    commit tree fe743d23e32e0fed6cce03327e1ac8596f89cd24

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Follow along with the examples here
+function say(greeting, firstName) {
+  console.log("i am here")
+  return `${greeting}, ${firstName}!`;
+} console.log(say("whattup", "biotch"))
